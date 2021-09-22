af3012a1bff9b9c1bf4725e87c79e445c54a3925
geekyprashant52 <61961514+geekyprashant52@users.noreply.github.com>
2021-09-22 14:18:00 +0530

    get added

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ let port = process.env.PORT || 5050;
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("This is Backend for Historical Data");
+});
+
 app.listen(port, () => {
   console.log(`Server started at ${port}`);
 });
